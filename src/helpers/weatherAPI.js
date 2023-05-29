f7af2c0f6669b8cc221979a4e015cd07221334fd
Axlvox <leandro.jlds@gmail.com>
2023-05-29 00:41:57 -0300

    Requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,22 @@
 // Remova os comentários a medida que for implementando as funções
+const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const link = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`;
+
+  try {
+    const response = await fetch(link);
+    const log = await response.json();
+
+    if (log.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+      return [];
+    }
+
+    console.log(log);
+  } catch {
+    console.error('Houve um erro de execução na busca por cidades');
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
